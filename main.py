aa367e73de8d8e6a200b06027950a1f14ab53672
warnakulasuriyap-cyber <warnakulasuriya.p@iisgalvanimi.edu.it>
2026-04-27 09:07:12 +0000

    fine

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-
+import pandas as pd
 
 app = FastAPI()
-
+df = pd.read_excel("/workspaces/PAGINA_LOGIN/dati.xlsx")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/") # Endpoint: punto in cui andiamo a richiamare il server web
@@ -25,3 +25,11 @@ def Controlla(username: str = Form(...), password: str = Form(...)):
         return {"messaggio": 1}
     else:
         return {"messaggio": 0}
+
+@app.post("/loginPandas")
+def ControllaPassword(username: str = Form(...), password: str = Form(...)):
+    risultato = df[(df["USER"]==username) & (df["NAME"]== password)]
+    if not risultato.empty :
+        return {"messaggio": 1}
+    else:
+        return {"messaggio" : 0}
--- static/script.js
+++ static/script.js
@@ -26,3 +26,34 @@ async function loggati() {
 
 document.getElementById('bottone').addEventListener('click', loggati);
 document.getElementById('bottone2').addEventListener('click', loggati);
+
+
+async function loggatiPandas() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (!username || !password)
+        return alert("Scrivi lo username e password");
+
+    const res = await fetch("/loginPandas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+    });
+
+    const json = await res.json();
+
+    if (json.messaggio == 1){
+        document.getElementById("risultato").innerText = "Accesso effettuato";
+    }
+    else {
+        document.getElementById("risultato").innerText = "Accesso negato";
+    }
+}
+
+document.getElementById('bottone').addEventListener('click', loggati);
+document.getElementById('bottone2').addEventListener('click', loggati);
+document.getElementById('bottonePandas').addEventListener('click', loggatiPandas);
